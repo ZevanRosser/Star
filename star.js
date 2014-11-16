1d0d5f03a63007b0e11bb360d2864938eaf2cf91
ZevanRosser <bendvent@gmail.com>
2014-11-16 15:18:12 -0500

    tweaking inheritance

--- star.js
+++ star.js
@@ -36,40 +36,32 @@
   
   // from backbone
   var extend = function(protoProps, staticProps) {
-    var parent = this;
-    var child;
-    
-    // The constructor function for the new subclass is either defined by you
-    // (the "constructor" property in your `extend` definition), or defaulted
-    // by us to simply call the parent's constructor.
+    var parent = this,
+        child, 
+        Surrogate;
+
     if (protoProps && has(protoProps, 'constructor')) {
       child = protoProps.constructor;
     } else {
       child = function(){ return parent.apply(this, arguments); };
     }
     
-    // Add static properties to the constructor function, if supplied.
     Star.extend(child, parent, staticProps);
     
-    // Set the prototype chain to inherit from `parent`, without calling
-    // `parent`'s constructor function.
-    var Surrogate = function(){ this.constructor = child; };
+    Surrogate = function(){ this.constructor = child; };
     Surrogate.prototype = parent.prototype;
     child.prototype = new Surrogate;
     
-    // Add prototype properties (instance properties) to the subclass,
-    // if supplied.
     if (protoProps) Star.extend(child.prototype, protoProps);
-    
-    // Set a convenience property in case the parent's prototype is needed
-    // later.
-    child.__super__ = parent.prototype;
+ 
+    child.prototype.sup = parent.prototype
     
     return child;
   };
   
   Star.Demo = function() {
     console.log('demo constructor');
+    this.init.apply(this, arguments);
   };
   
   Star.Demo.prototype = {
