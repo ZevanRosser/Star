2d8d682ad94324d993a1cf4a5efd19d7ba0e86c1
ZevanRosser <bendvent@gmail.com>
2014-11-23 02:15:18 -0500

    model re-written

--- astrix.js
+++ astrix.js
@@ -1,6 +1,6 @@
 (function() {
   
-   window.Star = window.S = function(obj) {
+  window.Star = window.S = function(obj) {
     if (obj.template){
       return S.View.extend.apply(S.View, arguments); 
     } else if (obj.data) {
@@ -61,32 +61,112 @@
     child.prototype = new Surrogate;
     
     if (protoProps) S.extend(child.prototype, protoProps);
-
+    
     child.prototype.sup = parent.prototype;
     
     return child;
   };
- 
-  S.Collection = function(){};
+  
+  S.Collection = function(arr){
+    this.length = 0;
+    Array.prototype.push.apply(this, elems);
+  };
+  
+  S.Collection.extend = extend;
+  
+  S.Collection.prototype = {
+    constructor : S.Collection
+    // ?
+  };
   
   S.O = function() {
     this.init.apply(this, arguments);
   };
+  
   S.O.extend = extend;
+  
   S.O.prototype = {
     constructor : S.Model
   };
-  S.O.extend = extend;
-   
-  S.Model = function() {};
-  S.Model.extend = extend;
-  S.Model.prototype = {
-    constructor : S.Model
+  
+  
+  S.Model = function() {
+    this.init.apply(this, arguments);
+    this.data = this.process(this.data);
   };
+  
   S.Model.extend = extend;
   
-  S.View = function() {};
+  S.Model.prototype = {
+    constructor : S.Model,
+    process : function(obj, keyPath) {
+      var i,
+          path, 
+          temp, 
+          key, 
+          setter,
+          bindsTo,
+          bound,
+          self,
+          leng;
+      
+      keyPath = keyPath ? keyPath + '.' : '';
+      
+      if(obj === null || typeof(obj) !== 'object') return obj;
+      
+      temp = obj.constructor(); 
+      
+      for(key in obj) {
+        if(obj.hasOwnProperty(key)) {
+          path = keyPath + key;
+          temp[key] = this.process(obj[key], path);
+          
+          (function(key, path) {
+            
+            if (Array.isArray(temp[key])){
+              // something else
+              temp[key] = new S.Collection(temp[key]);
+              
+            } else  {
+              setter = temp['$' + key] = function(val) {
+                self = temp['$' + key];
+                temp[key] = val;
+                bindsTo = self.$bindsTo;
+                leng = bindsTo.length;
+                
+                for (i = 0; i < leng; i++){
+                  bound = bindsTo[i];
+                  if (bound.type === 'html') {
+                    bound.node.innerHTML = val;
+                  } else {
+                    var attr = bound.node.getAttribute(bound.type);
+                    // props vs attributes problem
+                    bound.node.setAttribute(bound.type, val);
+                    bound.node[bound.type] = val;
+                  }
+                }
+                
+                //console.log('set', '"' + path + '"', '=', val);
+              }; 
+              setter.$bindsTo = [];
+            }
+            
+          })(key, path);
+          
+        }
+      }
+      
+      return temp;
+    }
+  };
+  
+  
+  S.View = function() {
+    this.init.apply(this, arguments);
+  };
+  
   S.View.extend = extend;
+  
   S.View.prototype = {
     constructor : S.View
   };
